306d0c221caa3540cbb0d8c18c070f319f850466
Sofia <sofict2006@gmail.com>
2025-03-25 11:23:58 -0500

    Se crea el token en dictators

---
+++ src/dictators/JwtStrategy.ts
@@ -0,0 +1,27 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "src/interface/JwtPayload";
+import { Dictator } from "./entities/dictator.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor(
+        @InjectRepository(Dictator)
+        private readonly dictatorRepository:Repository<Dictator>
+    ){
+        super({ jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(), secretOrKey: process.env.SECRET_KEY });
+    }
+    
+    async validate(payload:JwtPayload){
+       const {name} = payload;
+        const dictator=await this.dictatorRepository.findOneBy({name:name});
+        if(!dictator){
+                throw new Error('User not found');
+        }
+            return dictator;
+    }
+}
--- src/dictators/dictators.module.ts
+++ src/dictators/dictators.module.ts
@@ -4,11 +4,26 @@ import { DictatorsController } from './dictators.controller';
 import { Dictator } from './entities/dictator.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Slave } from 'src/slaves/entities/slave.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [DictatorsController],
   providers: [DictatorsService],
-  imports: [TypeOrmModule.forFeature([Dictator, Slave])],
+  imports: [TypeOrmModule.forFeature([Dictator, Slave]), 
+  PassportModule.register({ defaultStrategy: 'jwt'}),
+  JwtModule.registerAsync({
+    imports: [],
+    inject: [],
+    useFactory: () => {
+      return {
+        secret: process.env.SECRET_KEY,
+        signOptions: {
+          expiresIn: '1h'
+        }
+      }
+    }
+  })],
   exports: [DictatorsService, TypeOrmModule],
 })
 export class DictatorsModule {}
