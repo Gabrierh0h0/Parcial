69a1a40d975e4126a7c6f35ac4b83a18d523d458
Sofia <sofict2006@gmail.com>
2025-03-29 10:11:54 -0500

    Actualizacion

--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,33 +1,40 @@
-import { Controller, Get, Post, Patch, Delete, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Param, Body, UseGuards } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { Transaction } from './entities/transaction.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { AutenticadorGuard } from 'src/autenticador/autenticador.guard';
 
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @UseGuards(AuthGuard('jwt'), AutenticadorGuard)
   @Post()
   async create(@Body() createTransactionDto: CreateTransactionDto): Promise<Transaction> {
     return this.transactionsService.create(createTransactionDto);
   }
 
+  @UseGuards(AuthGuard('jwt'), AutenticadorGuard)
   @Get()
   async findAll(): Promise<Transaction[]> {
     return this.transactionsService.findAll();
   }
 
+  @UseGuards(AuthGuard('jwt'), AutenticadorGuard)
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Transaction> {
     return this.transactionsService.findOne(id);
   }
 
+  @UseGuards(AuthGuard('jwt'), AutenticadorGuard)
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto): Promise<Transaction> {
     return this.transactionsService.update(id, updateTransactionDto);
   }
 
+  @UseGuards(AuthGuard('jwt'), AutenticadorGuard)
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
     return this.transactionsService.remove(id);
--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -2,15 +2,17 @@ import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsService } from './transactions.service';
 import { Transaction } from './entities/transaction.entity';
-import { DictatorsModule } from '../dictators/dictators.module';
 import { TransactionsController } from './transactions.controller';
 import { Dictator } from 'src/dictators/entities/dictator.entity';
+import { DictatorsModule } from '../dictators/dictators.module';
 
 @Module({
-
-  controllers:[TransactionsController],
-  providers:[TransactionsService],
-  imports:[TypeOrmModule.forFeature([Transaction, Dictator]),DictatorsModule],
-  exports:[TypeOrmModule]
+  controllers: [TransactionsController],
+  providers: [TransactionsService],
+  imports: [
+    TypeOrmModule.forFeature([Transaction, Dictator]), DictatorsModule,
+    forwardRef(() => DictatorsModule), 
+  ],
+  exports: [TypeOrmModule],
 })
 export class TransactionsModule {}
